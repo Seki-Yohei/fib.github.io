004c1d366ff13242664f8862e0fa87c721ff4bc0
seki <68420226+Seki-Yohei@users.noreply.github.com>
2023-01-15 17:39:28 +0900

    Update Fibonacci.js

--- Fibonacci.js
+++ Fibonacci.js
@@ -7,43 +7,49 @@ function get_num() {
 }
 
 function fibonacci(fib) {
-  if (fib > 0)
+  try
   {
-    // フィボナッチ数列の1,2番目は1なので1を返す
-    if (fib == 1 || fib == 2)
+    if (fib > 0)
     {
-      return 1;
+      // フィボナッチ数列の1,2番目は1なので1を返す
+      if (fib == 1 || fib == 2)
+      {
+        return 1;
+      }
+      // それ以外の時には調べる
+      else
+      {
+        // 番号が既に調べ済みならば参照する
+        if (fib-2 in already)
+        {
+          var one = already[fib-2];
+        }
+        // まだ調べていなければ計算する
+        else
+        {
+          var one = fibonacci(fib-2);
+          already[fib-2] = one;
+        }
+        if (fib-1 in already)
+        {
+          var two = already[fib-1];
+        }
+        else
+        {
+          var two = fibonacci(fib-1);
+          already[fib-1] = two;
+        }
+        // 2つ前と1つ前の値の和を返す
+        return one + two;
+      }
     }
-    // それ以外の時には調べる
     else
     {
-      // 番号が既に調べ済みならば参照する
-      if (fib-2 in already)
-      {
-        var one = already[fib-2];
-      }
-      // まだ調べていなければ計算する
-      else
-      {
-        var one = fibonacci(fib-2);
-        already[fib-2] = one;
-      }
-      if (fib-1 in already)
-      {
-        var two = already[fib-1];
-      }
-      else
-      {
-        var two = fibonacci(fib-1);
-        already[fib-1] = two;
-      }
-      // 2つ前と1つ前の値の和を返す
-      return one + two;
+      return 0
     }
   }
-  else
+  catch(e)
   {
-    return 0
+    return Infinity
   }
-  
 }
