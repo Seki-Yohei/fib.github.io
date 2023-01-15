4ad46474984ba4ff60796696643259e11f30ed8f
seki <68420226+Seki-Yohei@users.noreply.github.com>
2023-01-15 17:31:52 +0900

    Update Fibonacci.js

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,49 +1,49 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>getの利用</title>
-  <script type="text/javascript" method="GET" src="Fibonacci.js"></script>
-</head>
-<body>
-<p>getメソッドでurlの末尾に「?n=なんかの数字」を足してフィボナッチ数列のn番目を見てみよう</p>
-<p id="f"></p>
-  
-</body>
-<script type="text/javascript">
-  // 既に計算したものを保存する辞書
-  const already = {};
-  // fibonacci関数で調べたものをfibとする
-  var fib = fibonacci(get_num());
-  // fibが規格外の大きさになった場合には
-  if (fib == "Infinity")
+function get_num() {
+  let url = new URL(window.location.href);
+  // URLSearchParamsオブジェクトを取得
+  let params = url.searchParams;
+  var fib = (parseInt(params.get('n'),10));
+  return fib;
+}
+
+function fibonacci(fib) {
+  if (fib > 0)
   {
-    // エラーを記載
-    const json = {
-      "status":416,
-      "message":"Range Not Satisfiable"
-    };
-    const ans = JSON.stringify(json);
-    document.getElementById("f").textContent = ans;
+    // フィボナッチ数列の1,2番目は1なので1を返す
+    if (fib == 1 || fib == 2)
+    {
+      return 1;
+    }
+    // それ以外の時には調べる
+    else
+    {
+      // 番号が既に調べ済みならば参照する
+      if (fib-2 in already)
+      {
+        var one = already[fib-2];
+      }
+      // まだ調べていなければ計算する
+      else
+      {
+        var one = fibonacci(fib-2);
+        already[fib-2] = one;
+      }
+      if (fib-1 in already)
+      {
+        var two = already[fib-1];
+      }
+      else
+      {
+        var two = fibonacci(fib-1);
+        already[fib-1] = two;
+      }
+      // 2つ前と1つ前の値の和を返す
+      return one + two;
+    }
   }
-  // fibが正しい形ならば
-  else if (get_num()>0)
-  {
-    // 結果を記載
-    const json = {"result":fib};
-    const ans = JSON.stringify(json);
-    document.getElementById("f").textContent = ans;
-  }
-  // fibが間違った形ならば
   else
   {
-    // エラーを記載
-    const json = {
-      "status":400,
-      "message":"Bad request.",
-    }
-    const ans = JSON.stringify(json);
-    document.getElementById("f").textContent = ans;
+    return 0
   }
-</script>
-</html>
+  
+}
